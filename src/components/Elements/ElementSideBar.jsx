46045f36a32c3ae433b6421784caa6cbc6e80d46
the invictus codes <realhannan0079@gmail.com>
2024-12-17 23:41:58 +0530

    Update ElementSideBar.jsx

--- src/components/Elements/ElementSideBar.jsx
+++ src/components/Elements/ElementSideBar.jsx
@@ -13,7 +13,7 @@ const ElementSideBar = ({ onSidebarItemClick }) => {
   return (
     
     <div className="">
-      <div className="md:mt-0 md:ml-[2%] md:w-[15%] font-semibold text-lg md:gap-y-4 flex md:flex-col flex-row flex-wrap  justify-start items-start p-4">
+      <div className=" md:mt-0 md:ml-[2%] md:w-[15%] font-semibold text-lg md:gap-y-4 md:gap-x-0 gap-3 flex md:flex-col flex-row flex-wrap  justify-start items-start p-4">
       {[
         { label: "All", icon: FaBorderAll },
         { label: "Header", icon: CiMenuFries },
@@ -27,7 +27,7 @@ const ElementSideBar = ({ onSidebarItemClick }) => {
         <div
           onClick={() => onSidebarItemClick(item.label)}
           key={index}
-          className="bg-neutral-800 md:w-[180px] w-[100px]  rounded-lg text-white pl-2 p-1 cursor-pointer hover:bg-neutral-700 flex  flex-wrap items-center gap-x-2"
+          className="bg-neutral-800 md:w-[180px] md:border-[0px] border border-neutral-500  rounded-lg text-white pl-2 p-1 cursor-pointer hover:bg-neutral-700 flex  flex-wrap justify-start items-center gap-x-2   "
         >
           <div>
             <item.icon className="text-xl" />
