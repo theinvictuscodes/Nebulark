20f5b0c7a6466136704af1619758ae7eff9ce014
the invictus codes <realhannan0079@gmail.com>
2024-12-17 22:58:23 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,10 @@ import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <header className="py-3 mt-[4px] ml-[2%]  w-[96%] bg-neutral-700 border border-neutral-600  rounded-lg flex md:flex-row flex-col md justify-evenly items-center text-xl  font-semibold ">
+
       <div className="md:text-4xl  md:mb-0  mb-4 font-bold text-red-700">Nebulark</div>
-      <div className=" flex justify-center items-center gap-x-8 md:gap-x-14 text-lg md:text-2xl ">
+
+      <div className=" flex justify-center items-center gap-x-2 md:gap-x-14 text-lg md:text-2xl ">
         
         <div className="hover:text-red-500 hover:cursor-pointer bg-neutral-600 rounded-md px-4  ">
           <Link to="/">Home</Link>
@@ -13,7 +15,7 @@ const Header = () => {
         
         <div className="hover:text-red-500 hover:cursor-pointer bg-neutral-600 rounded-md px-4  ">
           <Link to="/portfolio">
-            portfolio
+            portfolio 
           </Link>
         </div>
       
