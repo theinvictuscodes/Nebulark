d65513f3f3aa1e1759ad3c46134dccd34d789311
the invictus codes <realhannan0079@gmail.com>
2024-12-17 22:43:34 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,11 +11,9 @@ const Header = () => {
           <Link to="/">Home</Link>
         </div>
         
-        <div>
+        <div className="hover:text-red-500 hover:cursor-pointer bg-neutral-600 rounded-md px-4  ">
           <Link to="/portfolio">
-            <button class="flex  cursor-pointer text-white font-semibold bg-gradient-to-r from-gray-800 to-black px-2 md:px-7 py-2 md:py-3 rounded-full border border-gray-600 hover:scale-105 duration-200  hover:border-gray-800 hover:from-red-600 hover:to-red-300">
-              Portfolio Templates
-            </button>
+            Portfolio template
           </Link>
         </div>
       
